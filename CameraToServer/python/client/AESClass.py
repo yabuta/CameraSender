99b84ae941782924ecb677a1f38a18dd082459d5
yabuta <tefutefu1989@gmail.com>
2015-11-29 20:44:19 +0900

    add licence information for reference

--- CameraToServer/python/client/AESClass.py
+++ CameraToServer/python/client/AESClass.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+#Copyright (c) 2014 teitei_tk  
+#http://qiita.com/teitei_tk/items/0b8bae99a8700452b718
+#Released under the MIT license
+#http://opensource.org/licenses/mit-license.php
+
+
 import base64
 from Crypto import Random
 from Crypto.Cipher import AES
